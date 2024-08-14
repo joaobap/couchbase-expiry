eecc58ca55de18b7898bc9aa3afd75434599df0c
João Baptista <jbaptista@liveperson.com>
2024-08-14 17:19:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+
+## Usage:
+
+`couchbase43` contains couchbase 4.3.0
+`couchbase44` contains couchbase 4.4.0
+
+Run:
+
+`CB_HOST='couchbase://localhost' CB_USER=<user> CB_PASSWORD=<password> CB_BUCKET=<bucket> node index.mjs`
+
+Both scripts are the same apart from the document name.
+Expiry time is logged
---
+++ couchbase43/index.mjs
@@ -0,0 +1,22 @@
+import { env } from 'node:process';
+import { connect } from 'couchbase'
+
+async function main() {
+    const cluster = await connect(env.CB_HOST, {
+        username: env.CB_USER,
+        password: env.CB_PASSWORD,
+    });
+    const bucket = cluster.bucket(env.CB_BUCKET);
+    const collection = bucket.defaultCollection();
+    const doc = {
+        hello: "world",
+    };
+    const expiry = Math.floor(Date.now() / 1000) + 60 * 60;
+    const options = { expiry };
+    await collection.upsert('bar', doc, options);
+
+    const getRes = await collection.get('bar', { withExpiry: true });
+    console.log(getRes.expiryTime);
+}
+
+main().catch(error => console.log(error));
